11d0eb0edf21e1c48e17e89c248e763655424132
Rafał Florczak <florczak.raf@gmail.com>
2023-12-14 10:41:04 +0100

    Cache execution stats

--- bench.sh
+++ bench.sh
@@ -4,7 +4,12 @@ set -Eeuo pipefail
 N=10
 
 for s in $(find . -name '*py' | sort); do
-    echo -n "$s: "
+    if [ -f "$s.stats" ]; then
+        cat "$s.stats"
+        continue
+    fi
+
+    echo -n "$s: " >> "$s.stats"
     totals=()
     for _ in $(seq 1 $N); do
         d=$(dirname "$s")
@@ -12,14 +17,15 @@ for s in $(find . -name '*py' | sort); do
         python3 "$s" < "$d/input" >/dev/null
         total_ms="$((($(date +%s%N) - "$start")/1000000))"
         totals+=("$total_ms")
-        echo -n "$total_ms "
+        echo -n "$total_ms " >> "$s.stats"
     done
-    echo -n "ms | avg: "
+    echo -n "ms | avg: " >> "$s.stats"
 
     sum=0
     for e in "${totals[@]}"; do
         sum=$(("$sum" + "$e"))
     done
-    echo "$(("$sum" / "$N")) ms"
+    echo "$(("$sum" / "$N")) ms" >> "$s.stats"
 
+    cat "$s.stats"
 done
